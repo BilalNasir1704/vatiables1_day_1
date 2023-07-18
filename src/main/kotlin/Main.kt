d6a72ff469bb767876840f0a2a629d04d06eb75d
BilalNasir1704 <110089858+BilalNasir1704@users.noreply.github.com>
2023-07-18 20:00:47 +0300

    First intro

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,29 @@
+fun main() {
+//    println("Hello World")
+//    println("Hello World 2")
+//    println("Hello World 3")
+//    println("Hello World 4")
+//    println("Hello World 5")
+
+// println(1+2)
+//
+//    val x = 1
+//   // val y = 5 -- doesn't change
+//    var y=5
+//    y=70
+//    val z=x+y
+//    println("the answer is $z")
+//    println("the sum of $x and $y is $z")
+
+    val num1:Int = 12 //4 bytes
+    val num2:Long = 12 //8 bytes
+    val num3=num1+num2
+
+    val flo4: Float = .3f // 8bytes
+    val do4: Double = 0.3 // 16 bytes
+    print(flo4)
+
+    val c:Char='A' // 1 byte
+    val s:String="this is the kotlin class" //2 bytes
+
+}
